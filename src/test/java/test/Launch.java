315b5b6a3604cca71727fb0e732bc0dff1f39068
sachin.hb <sachin.hb@fireflink.com>
2024-12-10 13:32:50 +0530

    Sixth

--- src/test/java/test/Launch.java
+++ src/test/java/test/Launch.java
@@ -4,10 +4,9 @@ public class Launch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi");
-		System.out.println("Hello");
 		System.out.println("kjhgfds");
 		System.out.println("23456");
+		System.out.println("dfghj");
 	
 	}
 
